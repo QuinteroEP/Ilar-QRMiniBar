86338736190f0215624369df7883b7156bbe253c
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2026-04-20 13:54:32 -0500

    v1.1
    Despliegue por Vercel

---
+++ MiniBar/app.py
@@ -0,0 +1,19 @@
+from fastapi import FastAPI
+from controllers.product_controller import router as products_router
+from controllers.order_controller import router as orders_router
+from controllers.room_controller import router as room_router
+from models import ORM
+
+app = FastAPI()
+
+app.include_router(products_router, prefix="/api", tags=["Products"])
+app.include_router(orders_router, prefix="/api", tags=["Orders"])
+app.include_router(room_router, prefix="/api", tags=["Rooms"])
+
+@app.on_event("startup")
+def on_startup():
+    ORM.orm()
+
+@app.get("/")
+def root():
+    return {"root"}
