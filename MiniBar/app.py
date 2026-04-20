4cd8c83664b09b4d109a50a383a2baebf0ad387f
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2026-04-20 14:12:05 -0500

    v1.1.1
    CORS

--- MiniBar/app.py
+++ MiniBar/app.py
@@ -1,10 +1,18 @@
 from fastapi import FastAPI
+from fastapi.middleware.cors import CORSMiddleware
 from controllers.product_controller import router as products_router
 from controllers.order_controller import router as orders_router
 from controllers.room_controller import router as room_router
 from models import ORM
 
 app = FastAPI()
+
+app.add_middleware(
+    CORSMiddleware,
+    allow_origins=["*"],
+    allow_methods=["*"],
+    allow_headers=["*"],
+)
 
 app.include_router(products_router, prefix="/api", tags=["Products"])
 app.include_router(orders_router, prefix="/api", tags=["Orders"])
