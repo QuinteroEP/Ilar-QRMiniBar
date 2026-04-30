60c7e113fc25dfe1a6543acca44773ee1143484a
Pablo Enrique Quintero Callamand <140966677+QuinteroEP@users.noreply.github.com>
2026-04-30 12:23:50 -0500

    1.3.3
    validación de parametros

--- MiniBar/schemas/product_order_schema.py
+++ MiniBar/schemas/product_order_schema.py
@@ -1,8 +1,8 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 class ProductOrderSchema(BaseModel):
     roomId: float
     productId: float
-    quantity: int
+    quantity: int = Field(gt=1)
     model_config = ConfigDict(extra="forbid")
     
--- MiniBar/schemas/product_schema.py
+++ MiniBar/schemas/product_schema.py
@@ -1,7 +1,7 @@
-from pydantic import BaseModel, ConfigDict
+from pydantic import BaseModel, ConfigDict, Field
 
 class ProductSchema(BaseModel):
     name: str
-    price: float
-    inventory: int
+    price: float = Field(gt=0)
+    inventory: int = Field(ge=0)
     model_config = ConfigDict(extra="forbid")
